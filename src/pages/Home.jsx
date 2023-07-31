c667daf892ac707a30a61c8ef5cb6518bc6ef12c
crazycricketer003 <84874535+crazycricketer003@users.noreply.github.com>
2023-07-31 18:01:38 +0530

    commit_init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,181 +1,180 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Grid, colors } from "@mui/material";
 import Paper from "@mui/material/Paper";
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body1,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.primary,
-}));
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Box sx={{ flexGrow: 1 }}>
-        <Typography variant="h4" component="div">
-          Train Tracking
-        </Typography>
-        <br></br>
-        <Grid container spacing={1} justifyContent="center">
-          <Grid item xs={10}>
-            <Item>
-              <Typography variant="h7" component="div">
-                Train Details
-              </Typography>
-              <br></br>
-              <Grid container spacing={2} justifyContent="center">
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>Train Name</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      Chennai Exp
-                    </Typography>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>Train Number</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      2344
-                    </Typography>
-                  </Item>
-                </Grid>
-              </Grid>
-            </Item>
-          </Grid>
-          <Grid item xs={10}>
-            <Item>
-              <Typography variant="h7" component="div">
-                Timing Details
-              </Typography>
-              <br></br>
-              <Grid container spacing={2} justifyContent="center">
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>Timing</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      21:35:00
-                    </Typography>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>Delay</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      15
-                    </Typography>
-                  </Item>
-                </Grid>
-              </Grid>
-            </Item>
-          </Grid>
-          <Grid item xs={10}>
-            <Item>
-              <Typography variant="h7" component="div">
-                Seats
-              </Typography>
-              <br></br>
-              <Grid container spacing={2} justifyContent="center">
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>Sleeper</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      3
-                    </Typography>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>3AC</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      1
-                    </Typography>
-                  </Item>
-                </Grid>
-              </Grid>
-            </Item>
-          </Grid>
-          <Grid item xs={10}>
-            <Item>
-              <Typography variant="h7" component="div">
-                Price
-              </Typography>
-              <br></br>
-              <Grid container spacing={2} justifyContent="center">
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>Sleeper</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      2
-                    </Typography>
-                  </Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Item>
-                    <Typography>3AC</Typography>
-                    <Typography
-                      variant="h7"
-                      component="div"
-                      style={{ fontWeight: 200, color: "gray" }}
-                    >
-                      5
-                    </Typography>
-                  </Item>
-                </Grid>
-              </Grid>
-            </Item>
-          </Grid>
-        </Grid>
-      </Box>
-    </CardContent>
-  </React.Fragment>
-);
+import axios from "axios";
 
 export default function OutlinedCard() {
+  const [data, setUserData] = useState([]);
+
+  useEffect(() => {
+    const authToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA4MDYzNTMsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiNzMwNjQwYjMtZWRiZC00Njg1LThjMzItMGRkMDIyMDJhMjQ5Iiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjE2NyJ9.MKbm1WBKHZhT8q67RHaG9JTXV4ti0rYoSrnoHTWC6Hs";
+
+    const headers = {
+      Authorization: `Bearer ${authToken}`,
+    };
+
+    axios
+      .get("http://20.244.56.144/train/trains", { headers })
+      .then((response) => {
+        setUserData(response.data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined">{card}</Card>
-    </Box>
-  );
+      <Card variant="outlined">
+        <CardContent>
+          <Box sx={{ flexGrow: 1 }} style={{ backgroundColor: "grey" }}>
+            <Typography variant="h4" component="div">
+              Train Tracking
+            </Typography>
+            <br />
+            <Grid container spacing={1} justifyContent="center">
+              {data.map((train, index) => (
+                <Grid item xs={10} key={index}>
+                  <Paper>
+                    <Typography variant="h7" component="div">
+                      Train Details
+                    </Typography>
+                    <br />
+                    <Grid container spacing={2} justifyContent="center">
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>Train Name</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{ fontWeight: 200, color: "gray" }}
+                          >
+                            {train.trainName}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>Train Number</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{ fontWeight: 200, color: "gray" }}
+                          >
+                            {train.trainNumber}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                    </Grid>
+                    <Typography variant="h7" component="div">
+                      Timing Details
+                    </Typography>
+                    <br />
+                    <Grid container spacing={2} justifyContent="center">
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>Timing</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{ fontWeight: 200, color: "gray" }}
+                          >
+                            {train.departureTime.Hours}-
+                            {train.departureTime.Minutes}-
+                            {train.departureTime.Seconds}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>Delay</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{ fontWeight: 200, color: "gray" }}
+                          >
+                            {train.delayedBy}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                    </Grid>
+                    <Typography variant="h7" component="div">
+                      Seats
+                    </Typography>
+                    <br />
+                    <Grid container spacing={2} justifyContent="center">
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>Sleeper</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{ fontWeight: 200, color: "gray" }}
+                          >
+                            {train.seatsAvailable.sleeper}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>3AC</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{
+                              fontWeight: 200,
+                              color: "gray",
+                            }}
+                          >
+                            {train.seatsAvailable.AC}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                    </Grid>
+                    <Typography variant="h7" component="div">
+                      Price
+                    </Typography>
+                    <br />
+                    <Grid container spacing={2} justifyContent="center">
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>Sleeper</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{ fontWeight: 200, color: "gray" }}
+                          >
+                            {train.price.sleeper}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Paper>
+                          <Typography>3AC</Typography>
+                          <Typography
+                            variant="h7"
+                            component="div"
+                            style={{ fontWeight: 200, color: "gray" }}
+                          >
+                            {train.price.AC}
+                          </Typography>
+                        </Paper>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
